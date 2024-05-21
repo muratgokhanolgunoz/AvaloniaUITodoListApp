b9df0510a069e6627007fb19276ee16563780f29
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-05-21 11:11:46 +0300

    Add ToDoListService

---
+++ Services/ToDoListService.cs
@@ -0,0 +1,33 @@
+﻿using AvaloniaUITodoListApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaUITodoListApp.Services
+{
+    public class ToDoListService
+    {
+        public IEnumerable<ToDoItem> GetItems() => new[]
+        {
+            new ToDoItem
+            {
+                Description = "Update the project documentation"
+            },
+            new ToDoItem
+            {
+                Description = "Implement the login feature"
+            },
+            new ToDoItem
+            {
+                Description = "Fix bugs reported in issue tracker"
+            },
+            new ToDoItem
+            {
+                Description = "Review pull requests",
+                IsChecked = true
+            }
+        };
+    }
+}

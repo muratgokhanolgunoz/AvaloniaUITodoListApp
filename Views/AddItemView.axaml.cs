7ce62b9fea35b697793a779df36f07415181f56c
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-05-21 11:43:27 +0300

    Add AddItemView user control

---
+++ Views/AddItemView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvaloniaUITodoListApp.Views;
+
+public partial class AddItemView : UserControl
+{
+    public AddItemView()
+    {
+        InitializeComponent();
+    }
+}

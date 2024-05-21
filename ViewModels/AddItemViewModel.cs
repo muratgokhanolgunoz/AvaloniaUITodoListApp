82d9f7220ceef30397560179b792a031a2826d78
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-05-21 12:06:32 +0300

    Add button command for ADD and CANCEL behavior

--- ViewModels/AddItemViewModel.cs
+++ ViewModels/AddItemViewModel.cs
@@ -1,7 +1,30 @@
+using AvaloniaUITodoListApp.Models;
+using ReactiveUI;
+using System.Reactive;
+
 namespace AvaloniaUITodoListApp.ViewModels
 {
     public class AddItemViewModel : ViewModelBase
     {
-        public string Description { get; set; } = string.Empty;
+        private string _description = string.Empty;
+
+        public ReactiveCommand<Unit, ToDoItem> AddCommand { get; }
+        public ReactiveCommand<Unit, Unit> CancelCommand { get; }
+
+        public AddItemViewModel()
+        {
+            var isValidObservable = this.WhenAnyValue(
+                x => x.Description,
+                x => !string.IsNullOrWhiteSpace(x));
+
+            AddCommand = ReactiveCommand.Create(() => new ToDoItem { Description = Description }, isValidObservable);
+            CancelCommand = ReactiveCommand.Create(() => { });
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => this.RaiseAndSetIfChanged(ref _description, value);
+        }
     }
 }

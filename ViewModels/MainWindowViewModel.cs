8ae8b8c055e4a12236492ab6ce9402c068a4e598
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-05-21 11:55:58 +0300

    Add dynamic page transition feature at MainWindowViewModel

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,15 +1,31 @@
 ﻿using AvaloniaUITodoListApp.Services;
+using ReactiveUI;
 
 namespace AvaloniaUITodoListApp.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private ViewModelBase _viewModelBase;
+
         public MainWindowViewModel()
         {
             var service = new ToDoListService();
             ToDoListViewModel = new ToDoListViewModel(service.GetItems());
+
+            _viewModelBase = ToDoListViewModel;
         }
 
         public ToDoListViewModel ToDoListViewModel { get; }
+
+        public ViewModelBase ViewModelBase
+        {
+            get => _viewModelBase;
+            private set => this.RaiseAndSetIfChanged(ref _viewModelBase, value);
+        }
+
+        public void AddItem()
+        {
+            ViewModelBase = new AddItemViewModel();
+        }
     }
 }

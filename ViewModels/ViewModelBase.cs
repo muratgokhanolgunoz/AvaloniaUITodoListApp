3618420b9a09a723310e93f2031a84ab3f0cc557
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-05-21 10:37:01 +0300

    Add project files.

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaUITodoListApp.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaUITodoListApp.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

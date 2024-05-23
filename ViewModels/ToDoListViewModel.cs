876b41463bffe43ba5e193632b206578f49123b4
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-05-23 05:18:18 +0300

    Bug Fixed - Delete ToDoItem

--- ViewModels/ToDoListViewModel.cs
+++ ViewModels/ToDoListViewModel.cs
@@ -1,31 +1,32 @@
 using AvaloniaUITodoListApp.Models;
+using MsBox.Avalonia;
+using MsBox.Avalonia.Enums;
 using ReactiveUI;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reactive;
-using System.Windows.Input;
 
 namespace AvaloniaUITodoListApp.ViewModels
 {
     public class ToDoListViewModel : ViewModelBase
     {
         public ObservableCollection<ToDoItem> ToDoList { get; }
-        public ICommand DeleteCommand { get; }
+        public ReactiveCommand<int, Unit> DeleteCommand { get; }
 
         public ToDoListViewModel(IEnumerable<ToDoItem> items)
         {
             ToDoList = new ObservableCollection<ToDoItem>(items);
 
-            DeleteCommand = ReactiveCommand.Create<ToDoItem>(item =>
+            DeleteCommand = ReactiveCommand.Create<int>(id =>
             {
-                var list = items.ToList();
-                list.Remove(item);
+                ToDoItem? item = ToDoList.FirstOrDefault(x => x.Id == id);
 
-                items = list.AsEnumerable();
+                if(!(item is null))
+                {
+                    ToDoList.Remove(item);
+                }
             });
         }
-
-        
     }
 }

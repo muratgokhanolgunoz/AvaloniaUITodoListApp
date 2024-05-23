9d8ed36e8223283a635b23c3fddda3acc8a40503
Murat Gökhan Olgunöz <gokhanolgunoz@gmail.com>
2024-05-23 05:41:48 +0300

    Refactor - ToDoListViewModel delete process

--- ViewModels/ToDoListViewModel.cs
+++ ViewModels/ToDoListViewModel.cs
@@ -1,7 +1,10 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
 using AvaloniaUITodoListApp.Models;
 using MsBox.Avalonia;
 using MsBox.Avalonia.Enums;
 using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -12,21 +15,36 @@ namespace AvaloniaUITodoListApp.ViewModels
     public class ToDoListViewModel : ViewModelBase
     {
         public ObservableCollection<ToDoItem> ToDoList { get; }
-        public ReactiveCommand<int, Unit> DeleteCommand { get; }
+        public ReactiveCommand<int, Unit> DeleteItemCommand { get; }
 
         public ToDoListViewModel(IEnumerable<ToDoItem> items)
         {
             ToDoList = new ObservableCollection<ToDoItem>(items);
+            DeleteItemCommand = ReactiveCommand.Create<int>(DeleteItemProcess);
+        }
 
-            DeleteCommand = ReactiveCommand.Create<int>(id =>
+        public async void DeleteItemProcess(int id)
+        {
+            var messageBoxConfig = MessageBoxManager.GetMessageBoxStandard("Delete Process", "Are you sure?", ButtonEnum.YesNo, Icon.Question);
+            var messageBox = await messageBoxConfig.ShowWindowDialogAsync((Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow);
+
+            if (messageBox == ButtonResult.Yes)
             {
-                ToDoItem? item = ToDoList.FirstOrDefault(x => x.Id == id);
-
-                if(!(item is null))
+                try
                 {
-                    ToDoList.Remove(item);
+                    ToDoItem? item = ToDoList.FirstOrDefault(item => item.Id == id);
+
+                    if (!(item is null))
+                    {
+                        ToDoList.Remove(item);
+                    }
                 }
-            });
+                catch (Exception exception)
+                {
+                    var messageBoxErrorConfig = MessageBoxManager.GetMessageBoxStandard("Error", $"Failed to delete item. (Error: {exception})", ButtonEnum.Ok, Icon.Error);
+                    await messageBoxErrorConfig.ShowWindowDialogAsync((Application.Current?.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow);
+                }
+            }
         }
     }
 }
